7f76809dcabb5fa91b4b373c38da9e348fab69a4
Da1man <chaos4ever@mail.ru>
2020-04-21 14:08:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Car from './Car/Car'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'Ford', year: 2018},
+      {name: 'Audi', year: 2016},
+      {name: 'Mazda 1', year: 2010},
+    ],
+    pageTitle: 'React components',
+    showCars: false,
+  };
+
+  onChangeName(name, index){
+    const car = this.state.cars[index];
+    car.name = name;
+    const cars = [...this.state.cars];
+    cars[index] = car
+    this.setState({
+      cars,
+    })
+  }
+
+  toggleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars,
+    })
+  };
+
+  deleteHandler (index){
+    const cars = this.state.cars.concat();
+
+    cars.splice(index,1);
+    this.setState({cars})
+  }
+
+  render() {
+    const divStyle = {
+      textAlign: 'center',
+    };
+    let cars = null
+
+    if (this.state.showCars) {
+      cars = this.state.cars.map((car, index) => {
+        return(
+          <Car
+            key={index}
+            name={car.name}
+            year={car.year}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
+            onDelete={this.deleteHandler.bind(this, index)}
+          />
+        )
+      })
+    }
+    return (
+      <div style={divStyle}>
+        <h1>{this.state.pageTitle}</h1>
+
+        <button
+          onClick={this.toggleCarsHandler}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Toggle Cars
+        </button>
+
+        {cars}
+      </div>
+    );
+  }
 }
 
 export default App;
