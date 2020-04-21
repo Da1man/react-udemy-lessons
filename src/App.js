ba64058275c26bdda02eb2b61e0042f09914eb79
Da1man <chaos4ever@mail.ru>
2020-04-21 20:26:10 +0300

    lesson 34 done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
 import Car from './Car/Car'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
 
-  state = {
-    cars: [
-      {name: 'Ford', year: 2018},
-      {name: 'Audi', year: 2016},
-      {name: 'Mazda 1', year: 2010},
-    ],
-    pageTitle: 'React components',
-    showCars: false,
-  };
+  constructor(props) {
+    // console.log('App constructor')
+    super(props)
+
+    this.state = {
+      cars: [
+        {name: 'Ford', year: 2018},
+        {name: 'Audi', year: 2016},
+        {name: 'Mazda 1', year: 2010},
+      ],
+      pageTitle: 'React components',
+      showCars: false,
+    };
+  }
 
   onChangeName(name, index){
     const car = this.state.cars[index];
@@ -37,7 +43,16 @@ class App extends Component {
     this.setState({cars})
   }
 
+  // componentWillMount() {
+  //   console.log('App componentWillMount')
+  // }
+
+  // componentDidMount() {
+  //   console.log('App componentDidMount')
+  // }
+
   render() {
+    // console.log('App render')
     const divStyle = {
       textAlign: 'center',
     };
@@ -46,22 +61,33 @@ class App extends Component {
     if (this.state.showCars) {
       cars = this.state.cars.map((car, index) => {
         return(
+          <ErrorBoundary key={index} >
           <Car
-            key={index}
             name={car.name}
             year={car.year}
             onChangeName={event => this.onChangeName(event.target.value, index)}
             onDelete={this.deleteHandler.bind(this, index)}
           />
+          </ErrorBoundary>
         )
       })
     }
     return (
       <div style={divStyle}>
-        <h1>{this.state.pageTitle}</h1>
+        {/*<h1>{this.state.pageTitle}</h1>*/}
+        <h1>{this.props.title}</h1>
 
         <button
           onClick={this.toggleCarsHandler}
+          style={{
+            fontWeight: 900,
+            padding: 15,
+            backgroundColor: "#ffffff",
+            textTransform: 'uppercase',
+            border: '2px solid #ccc',
+            cursor: 'pointer',
+            outline: 'none',
+          }}
         >
           Toggle Cars
         </button>
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,40 +1,83 @@
 import React from 'react';
-import Radium from 'radium';
+// import Radium from 'radium';
 import './Car.css'
 
-const Car = (props) => {
-  const inputClasses = ['input']
-  if (props.name !== '') {
-    inputClasses.push('green')
-  } else {
-    inputClasses.push('red')
-  }
 
-  if (props.name.length > 4) {
-    inputClasses.push('bold')
-  }
+class Car extends React.Component {
 
-  const style = {
-    border: '1px solid #ccc',
-    boxShadow: '0 4px 5px 0 rgba(0,0,0,.14)',
-    ':hover': {
-      border: '1px solid #aaa',
-      boxShadow: '0 4px 15px 0 rgba(0,0,0,.55)',
-      cursor: 'pointer',
+  // componentWillReceiveProps(nextProps, nextContext) {
+  //   console.log('Car componentWillReceiveProps', nextProps, nextContext)
+  // }
+  //
+  // shouldComponentUpdate(nextProps, nextState, nextContext) {
+  //   console.log('Car shouldComponentUpdate', nextProps, nextState, nextContext)
+  //   return nextProps.name.trim() !== this.props.name.trim()
+  // }
+  //
+  // componentWillUpdate(nextProps, nextState, nextContext) {
+  //   console.log('Car componentWillUpdate', nextProps, nextState, nextContext)
+  // }
+  //
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   console.log('Car getDerivedStateFromProps', nextProps, prevState)
+  //
+  //   return prevState
+  // }
+  //
+  // componentDidUpdate(prevProps, prevState, snapshot) {
+  //   console.log('Car componentDidUpdate', prevProps, prevState, snapshot)
+  // }
+  //
+  // getSnapshotBeforeUpdate(prevProps, prevState) {
+  //   console.log('Car getSnapshotBeforeUpdate')
+  // }
+  //
+  // componentWillUnmount() {
+  //   console.log('Car componentWillUnmount')
+  // }
+
+
+  render() {
+    console.log('Car render')
+
+    if (Math.random() > 0.7){
+      throw new Error('Car random failed')
     }
-  };
 
-  return (
-    <div className={'Car'} style={style}>
-      <h3>Car name: {props.name}</h3>
-      <p>Year: <strong>{props.year}</strong></p>
-      <input
-        className={inputClasses.join(' ')}
-        type={'text'}
-        onChange={props.onChangeName}
-        value={props.name}/>
-      <button onClick={props.onDelete}>Delete</button>
-    </div>
-  )
+    const inputClasses = ['input']
+    if (this.props.name !== '') {
+      inputClasses.push('green')
+    } else {
+      inputClasses.push('red')
+    }
+
+    if (this.props.name.length > 4) {
+      inputClasses.push('bold')
+    }
+
+    const style = {
+      border: '1px solid #ccc',
+      boxShadow: '0 4px 5px 0 rgba(0,0,0,.14)',
+      ':hover': {
+        border: '1px solid #aaa',
+        boxShadow: '0 4px 15px 0 rgba(0,0,0,.55)',
+        cursor: 'pointer',
+      }
+    };
+
+    return (
+      <div className={'Car'} style={style}>
+        <h3>Car name: {this.props.name}</h3>
+        <p>Year: <strong>{this.props.year}</strong></p>
+        <input
+          className={inputClasses.join(' ')}
+          type={'text'}
+          onChange={this.props.onChangeName}
+          value={this.props.name}/>
+        <button onClick={this.props.onDelete}>Delete</button>
+      </div>
+    )
+  }
 }
-export default Radium(Car);
+
+export default Car;
