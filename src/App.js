8625a87ef4bc0752bd2a9c222273536587fe9687
Da1man <chaos4ever@mail.ru>
2020-04-24 17:52:58 +0300

    lesson 67 done

--- src/App.js
+++ src/App.js
@@ -1,117 +1,72 @@
-import React, {Component} from 'react';
-import './App.css';
-import Radium from "radium";
-import Car from './Car/Car';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
-import Counter from './Counter/Counter';
-
-export const ClickedContext = React.createContext(false)
+import React, {Component} from 'react'
+import './App.scss'
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+import CarDetail from "./CarDetail/CarDetail";
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      cars: [
-        {name: 'Ford', year: 2018},
-        {name: 'Audi', year: 2016},
-        {name: 'Mazda 1', year: 2010},
-      ],
-      pageTitle: 'React components',
-      showCars: false,
-      clicked: false,
-    };
-  }
-
-  onChangeName(name, index){
-    const car = this.state.cars[index];
-    car.name = name;
-    const cars = [...this.state.cars];
-    cars[index] = car
-    this.setState({
-      cars,
-    })
-  }
-
-  toggleCarsHandler = () => {
-    this.setState({
-      showCars: !this.state.showCars,
-    })
+  state = {
+    isLoggedIn: false,
   };
 
-  deleteHandler (index){
-    const cars = this.state.cars.concat();
-
-    cars.splice(index,1);
-    this.setState({cars})
-  }
-
   render() {
-    const divStyle = {
-      textAlign: 'center',
-    };
-    let cars = null
 
-    if (this.state.showCars) {
-      cars = this.state.cars.map((car, index) => {
-        return(
-          <ErrorBoundary key={index} >
-          <Car
-            name={car.name}
-            year={car.year}
-            index={index}
-            onChangeName={event => this.onChangeName(event.target.value, index)}
-            onDelete={this.deleteHandler.bind(this, index)}
-          />
-          </ErrorBoundary>
-        )
-      })
-    }
     return (
-      <div style={divStyle}>
-        {/*<h1>{this.state.pageTitle}</h1>*/}
-        <h1>{this.props.title}</h1>
-        <ClickedContext.Provider value={this.state.clicked}>
-          <Counter />
-        </ClickedContext.Provider>
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                exact
+                activeClassName={'wfm-active'}
 
-
+              >Home</NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                activeStyle={{
+                  color: 'blue',
+                }}
+              >About</NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={{
+                  pathname: '/cars',
+                  // search: '?a=1&b=2',
+                  // hash: 'wfm-hash',
+                }}
+              >Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
         <hr/>
-
-        <button
-          onClick={this.toggleCarsHandler}
-          style={{
-            fontWeight: 900,
-            padding: 15,
-            backgroundColor: "#ffffff",
-            textTransform: 'uppercase',
-            border: '2px solid #ccc',
-            cursor: 'pointer',
-            outline: 'none',
-            marginTop: 20,
-            ':hover' : {
-              backgroundColor: "#e7e7e7",
-            }
-          }}
-        >
-          Toggle Cars
-        </button>
-
-        <button onClick={() => this.setState({clicked: true})}>Changed Clicked</button>
-
-
-        <div style={{
-          width: 400,
-          margin: 'auto',
-          paddingTop: '20px',
-        }}>
-          {cars}
+        <div style={{textAlign: 'center'}}>
+          <h3>Is logged in: {this.state.isLoggedIn ? 'True' : 'False'}</h3>
+          <button onClick={() => this.setState({isLoggedIn: true})}>Log in</button>
         </div>
+        <hr/>
+        <Switch>
+          <Route path={'/'} exact render={() => <h1>Home Page</h1>}/>
+          {this.state.isLoggedIn
+            ? <Route path={'/about'} component={About}/>
+            : null
+          }
+          <Route path={'/cars/:name'} component={CarDetail}/>
+          <Route path={'/cars'} component={Cars}/>
+          <Redirect to={'/'}/>
+          {/*<Route*/}
+          {/*  render={() => <h1 style={{color: 'red', textAlign:'center'}}*/}
+          {/*  >404 not found</h1>}/>*/}
+        </Switch>
 
       </div>
     );
   }
 }
 
-export default Radium (App);
+export default App
